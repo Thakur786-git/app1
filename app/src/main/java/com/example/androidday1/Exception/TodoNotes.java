5e9a700afd425f9c2825d420b0574d2f0f5556ad
Thakur-786 <thakursantoshsing.bajajfinserv.in>
2021-07-14 14:03:50 +0530

    TodoNotes

---
+++ app/src/main/java/com/example/androidday1/Exception/TodoNotes.java
@@ -0,0 +1,14 @@
+package com.example.androidday1.Exception;
+public class TodoNotes(){
+
+    @Override
+    public String toString() {
+        return "TodoNote{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+        return title +"\n"+description;
+    }
+}
+}
